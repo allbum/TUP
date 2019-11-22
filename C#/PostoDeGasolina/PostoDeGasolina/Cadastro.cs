b8bc3efaa23f59024f4a119b333d334be679d552
AllanNotebook <allanbum@gmail.com>
2019-11-22 14:58:01 -0300

    commi gas store

---
+++ C#/PostoDeGasolina/PostoDeGasolina/Cadastro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostoDeGasolina
+{
+    public class Cadastro
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string TipoDePagamento { get; set; }
+    }
+}
